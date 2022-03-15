72d522e69ee433991a980c881f8ee9529761ed30
sola <827757772@qq.com>
2022-03-15 23:16:07 +0800

    grpc: multithreaded bombing.

--- io/network/grpc/grpc-client/src/main/java/com/wethura/learn/grpc/GrpcClient.java
+++ io/network/grpc/grpc-client/src/main/java/com/wethura/learn/grpc/GrpcClient.java
@@ -7,11 +7,28 @@ import com.wethura.learn.grpc.core.auto.HelloMessage.HelloResponse;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GrpcClient {
 
-    public static void main(String[] args) {
-        new GrpcClient().remoteCall("Jessy!");
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for(int i = 0; i < 10; i ++) {
+            int clientNo = i;
+            executorService.submit(() -> {
+                GrpcClient client = new GrpcClient();
+
+                while(true) {
+                    client.remoteCall(String.format("client-%02d", clientNo));
+                }
+            });
+        }
+
+        TimeUnit.SECONDS.sleep(60);
     }
 
     public void remoteCall(String name) {
