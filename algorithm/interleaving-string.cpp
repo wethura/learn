98953e06afffae26fc4e5e959caafb3c09cd98a0
sola <827757772@qq.com>
2022-03-29 22:57:21 +0800

    algorithm: add leetcode 97

---
+++ algorithm/interleaving-string.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include "time.h"
+using namespace std;
+
+template<typename T> void logger(T e);
+template<typename T> void logger(vector<T> arr);
+template<typename T> void logger(vector<vector<T>> arrs);
+
+/**
+ * coding here
+ */
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+        vector<vector<bool>> dp(l1 + 1, vector<bool>(l2 + 1, false)); dp[0][0] = true;
+        for(int i = 0; i <= l1; i ++) {
+            for(int j = 0; j <= l2; j ++) {
+                if(i && s1[i - 1] == s3[i + j - 1]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j];
+                }
+
+                if(j && s2[j - 1] == s3[i + j - 1]) {
+                    dp[i][j] = dp[i][j - 1] || dp[i][j];
+                }
+            }
+        }
+
+        // logger(dp);
+
+        return dp[l1][l2];
+    }
+};
+
+
+int main() {
+    Timer timer("Execute timer");
+    timer.restart();
+
+    Solution s;
+    logger(s.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+    timer.log("Program execute");
+}
+
+template<typename T> void logger(T e) {
+    cout << e << endl;
+}
+
+template<typename T> void logger(vector<T> arr) {
+    for_each(arr.begin(), arr.end(), [](T &e){cout << e << " ";});
+    cout << endl << endl;;
+}
+
+template<typename T> void logger(vector<vector<T>> arrs) {
+    for_each(arrs.begin(), arrs.end(), [](vector<T> &arr) {
+        for_each(arr.begin(), arr.end(), [](T e){cout << e << " ";});
+        cout << endl;
+    });cout << endl;
+}
