299fcb041a9a86d840cf255040b9404ff960d419
sola <827757772@qq.com>
2022-03-29 23:10:58 +0800

    algorithm: add leetcode 98

---
+++ algorithm/validate-binary-search-tree.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+#include "time.h"
+using namespace std;
+
+template<typename T> void logger(T e);
+template<typename T> void logger(vector<T> arr);
+template<typename T> void logger(vector<vector<T>> arrs);
+
+/**
+ * coding here
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    bool dfs(TreeNode* rt, long &cnt) {
+        if(rt -> left) {
+            if(!dfs(rt -> left, cnt)) {
+                return false;
+            }
+        }
+
+        if(cnt >= rt->val) {
+            return false;
+        } else {
+            cnt = rt -> val;
+        }
+
+        if(rt -> right) {
+            if(!dfs(rt -> right, cnt)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        long tmp = -(1 << 32);
+        return dfs(root, tmp);
+    }
+};
+
+
+int main() {
+    Timer timer("Execute timer");
+    timer.restart();
+
+    // Solution s;
+
+    // vector<int> arr = {1, 2, 3, 4};
+    // vector<char> arr = {'a', 'b', 'c', 'd'};
+
+    // vector<vector<int>> arrs = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+    // vector<vector<char>> arrs = {{'a', 'b', 'c', 'd'}, {'e', 'f', 'g', 'h'}};
+
+
+    // vector<int> res = s.resolve();
+    // vector<vector<int>> res = s.resolve();
+    // vector<char> res = s.resolve();
+    // vector<vector<char>> res = s.resolve();
+
+    timer.log("Program execute");
+
+
+    // logger(res);
+}
+
+template<typename T> void logger(T e) {
+    cout << e << endl;
+}
+
+template<typename T> void logger(vector<T> arr) {
+    for_each(arr.begin(), arr.end(), [](T &e){cout << e << " ";});
+    cout << endl << endl;;
+}
+
+template<typename T> void logger(vector<vector<T>> arrs) {
+    for_each(arrs.begin(), arrs.end(), [](vector<T> &arr) {
+        for_each(arr.begin(), arr.end(), [](T &e){cout << e << " ";});
+        cout << endl;
+    });cout << endl;
+}
