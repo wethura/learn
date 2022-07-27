554e720b5369241e0bf3f3dfcb37c049a993d7e9
wethura <wuweihua@bingosoft.net>
2022-07-27 10:59:57 +0800

    code: remove useless code.

--- spring/spring-demo/src/main/java/com/wethura/spring/Application.java
+++ spring/spring-demo/src/main/java/com/wethura/spring/Application.java
@@ -3,13 +3,11 @@ package com.wethura.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
  * @author wethura
  * @date 2021/1/25 上午3:55
  */
-@EnableRedisRepositories
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
